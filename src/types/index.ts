c92dc0d1e25d092c21eb9ce2991cf2ecde455be2
NahnDev <nahn.thanhnhan@gmail.com>
2024-06-12 00:39:12 +0700

    feat: Remove unused type `nil<T>`

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,6 @@
 import { TShape } from "@/components/share/maker/type";
 
+export type nil<T> = undefined | null | T;
+
 export type TSize = { width: number; height: number };
 export type TCoord = { x: number; y: number };
